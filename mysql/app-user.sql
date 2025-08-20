3d8cb9782c0d29613c6f3d989194b9593f748929
ajay <vallala1602@gmail.com>
2025-08-20 23:28:47 +0530

    docker

--- mysql/app-user.sql
+++ mysql/app-user.sql
@@ -2,3 +2,7 @@ CREATE USER IF NOT EXISTS 'shipping'@'%' IDENTIFIED WITH mysql_native_password B
 GRANT ALL ON cities.* TO 'shipping'@'%';
 FLUSH PRIVILEGES;
 
+-- Create 'shipping' user and grant access to 'cities' database
+CREATE USER IF NOT EXISTS 'shipping'@'%' IDENTIFIED WITH mysql_native_password BY 'RoboShop@1';
+GRANT ALL PRIVILEGES ON cities.* TO 'shipping'@'%';
+FLUSH PRIVILEGES;
---
+++ mysql/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -f /tmp/mysql_root_password.txt ] then
+  PASSWORD=$(cat /tmp/mysql_root_password.txt)
+  echo "Root password accessed"
+  else
+   echo "Password file not found"
+   exit 1
+if
+
+export MYSQL_ROOT_PASSWORD=$PASSWORD
+
+rm -rf /tmp/mysql_root_password.txt
+
+exec /entrypoint.sh mysqld
